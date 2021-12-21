4a91ed86b6a2a8247d300da5a81eaef682c2f153
Judit Ferenczik <jferenczik@gmail.com>
2021-12-21 22:26:45 +0100

    save into txt the missed states

--- main.py
+++ main.py
@@ -16,13 +16,22 @@ myTurtle = turtle.Turtle()
 #turtle.onscreenclick(get_mouse_click_coor)
 #turtle.mainloop()
 data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
 guesses = 0
 myTurtle.penup()
 previous_guesses = []
-while guesses < 5:
+while guesses < 50:
     answer_state = screen.textinput(title=f"{guesses}/50 Guess the state", prompt="What's another state's name?")
     good_guess = data[(data.state).str.lower() == answer_state.lower()]
     newCity = good_guess["state"].str.cat(sep='\n')
+    if answer_state.lower() == "exit":
+        missing_states = ""
+        for item in all_states:
+            if item.lower() not in previous_guesses:
+                missing_states += f"{item}\n"
+        with open("states_to_learn.txt", mode='w+') as dw:
+            dw.write(missing_states)
+        break
 #not a state's name or already considered
     if len(good_guess) != 0 and (newCity.lower() not in previous_guesses):
         myTurtle.goto(int(good_guess.x), int(good_guess.y))
